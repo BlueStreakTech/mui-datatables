a72a06616e2389c90683b3d82f14c3ca3321ed99
Truong Vo <tvo@bstek.net>
2025-08-05 10:46:47 -0500

    Updated customHeadRender parameters.

--- src/components/TableHead.js
+++ src/components/TableHead.js
@@ -128,7 +128,7 @@ const TableHead = ({
           ({ column, index, colPos }) =>
             column.display === 'true' &&
             (column.customHeadRender ? (
-              column.customHeadRender({ index, ...column, colPos, setCellRef, options, updateColumnOrder, columnOrder, timers, draggingHook: [dragging, setDragging], draggableHeadCellRefs, tableRef, tableId }, handleToggleColumn, sortOrder)
+              column.customHeadRender({ index, ...column, colPos, }, handleToggleColumn, sortOrder, { columns, column, setCellRef, options, updateColumnOrder, columnOrder, timers, draggingHook: [dragging, setDragging], draggableHeadCellRefs, tableRef, tableId, components })
             ) : (
               <TableHeadCell
                 cellHeaderProps={
